fc08aeff3c4aead952c0a2c3111eb2940a5528ae
Bruno Seiji <bruno_seiji_1995@hotmail.com>
2019-08-10 11:54:13 -0300

    Aula de Atribuição e Tenário

---
+++ atribuicao-e-ternario/script-aula.js
@@ -0,0 +1,20 @@
+// Some 500 ao valor de scroll abaixo, atribuindo o novo valor a scroll
+let scroll = 1000;
+let totalScroll = scroll += 500;
+
+console.log(totalScroll);
+
+// Atribua true para a variável darCredito,
+// Caso o cliente possua carro e casa.
+// E false caso o contrário.
+let possuiCarro = true;
+let possuiCasa = true;
+let darCredito;
+
+if(possuiCarro && possuiCasa)
+    darCredito = true;
+else
+    darCredito = false;
+
+console.log(darCredito);
+
---
+++ atribuicao-e-ternario/script.js
@@ -0,0 +1,23 @@
+let numero = 20;
+let numero2 = 10;
+
+numero += numero2; // numero = numero + numero2
+console.log(numero);
+// --------------------------------------------------
+
+let idade = 10;
+let podeBeber;
+
+podeBeber  = (idade >= 18 ) ? 'Pode beber' : 'Não pode beber';
+console.log(podeBeber);
+
+// --------------------------------------------------
+
+var possuiFaculdade = false;
+
+if (possuiFaculdade)
+    console.log('Sim, possui');
+else
+    console.log('Não possui');
+
+// --------------------------------------------------
