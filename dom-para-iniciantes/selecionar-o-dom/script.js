6c8df91f024c1e7d3cd121c8d90ac0e6bcaad948
Bruno Seije <git368@idealtrends.local>
2019-08-16 10:33:00 -0300

    Exercícios em andamento

--- dom-para-iniciantes/selecionar-o-dom/script.js
+++ dom-para-iniciantes/selecionar-o-dom/script.js
@@ -1,2 +1,35 @@
 const animais = document.getElementById('animais');
 console.log(animais);
+
+const gridSection = document.getElementsByClassName('grid-section');
+console.log(gridSection[0]);
+
+const primeiraLi = document.querySelector('li');
+console.log(primeiraLi);
+
+const primeiraUl = document.querySelector('ul');
+console.log(primeiraUl);
+
+const linkInterno = document.querySelector('[href^="#"]');
+console.log(linkInterno.href);
+
+const animaisImg = document.querySelectorAll('.animais img');
+console.log(animaisImg);
+
+const gridSectionHTML = document.getElementsByClassName('grid-section');
+const gridSectionNode = document.querySelectorAll('.grid-section');
+
+primeiraUl.classList.add('grid-section');
+
+// console.log(gridSectionHTML);
+// console.log(gridSectionNode);
+
+gridSectionNode.forEach(function(item, index){
+   console.log(item);
+});
+
+const arrayGrid = Array.from(gridSectionHTML);
+
+arrayGrid.forEach(function(item){
+   console.log(item);
+});
