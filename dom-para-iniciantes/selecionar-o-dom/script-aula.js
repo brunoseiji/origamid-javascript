ef7ba771ffd16760db33d3c2d5a298186a031604
Bruno Seiji <bruno_seiji_1995@hotmail.com>
2019-08-16 22:49:17 -0300

    Exercícios de seleção completo

--- dom-para-iniciantes/selecionar-o-dom/script-aula.js
+++ dom-para-iniciantes/selecionar-o-dom/script-aula.js
@@ -4,8 +4,9 @@ console.log('---------- Exercícios abaixo ----------')
 const todasImagens = document.querySelectorAll('img');
 console.log(todasImagens);
 
-// Retorne no console apenas as imagens que começaram com a palavra
-
+// Retorne no console apenas as imagens que começaram com a palavra imagem
+const imagensAnimais = document.querySelectorAll('img[src^="img/imagem"]');
+console.log(imagensAnimais);
 
 // Selecione todos os links internos (onde o href começa com #)
 const linksInternos = document.querySelectorAll('[href^="#"]');
@@ -16,5 +17,5 @@ const primeiroH2 = document.querySelector('.animais-descricao h2');
 console.log(primeiroH2);
 
 // Selecione o último p do site
-const ultimoP = document.getElementsByTagName('p:last-child');
-console.log(ultimoP);
+const ultimoP = document.querySelectorAll('p');
+console.log(ultimoP[ultimoP.length - 1]);
