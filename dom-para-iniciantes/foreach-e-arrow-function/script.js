2088fbd776f83f507dc53797d4169b4eb6729a5c
Bruno Seiji <bruno_seiji_1995@hotmail.com>
2019-08-19 00:18:30 -0300

    Exercícios de arrow functions e início de classes

---
+++ dom-para-iniciantes/foreach-e-arrow-function/script.js
@@ -0,0 +1,12 @@
+// const titulos = document.getElementsByClassName('titulo');
+// const titulosArray = Array.from(titulos);
+
+// titulosArray.forEach(function(item){
+//    console.log(item)
+// });
+
+const imgs = document.querySelectorAll('img');
+
+imgs.forEach((item) => {
+   console.log(item);
+});
