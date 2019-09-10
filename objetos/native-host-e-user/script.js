791204b86b7adaaf5ac04ee21056278e99007e3d
Bruno Seiji <bruno_seiji_1995@hotmail.com>
2019-09-09 23:05:01 -0300

    Objetos Native, Host e User 1

---
+++ objetos/native-host-e-user/script.js
@@ -0,0 +1,11 @@
+// Objetos Nativos
+// Object, String, Array, Function
+
+
+// Objetos do Host (Browser)
+// NodeList, HTMLCollection, Element
+
+// Objetos User
+// const Pessoa = {
+//     nome: 'Bruno';
+// }
