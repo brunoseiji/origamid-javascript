694157722a49f0a93cdcf2516305881ee70394e9
Bruno Seije <git368@idealtrends.local>
2019-09-23 10:53:00 -0300

    Array 2

--- objetos/array/script.js
+++ objetos/array/script.js
@@ -45,8 +45,34 @@ carros; // ['Honda', 'Kia', 'Ford', 'Fiat', 'VW'];
 carros.push('Ferrari'); // 6
 carros; // ['Honda', 'Kia', 'Ford', 'Fiat', 'VW', 'Ferrari'];
 
-console.log(carros.pop());
-console.log(carros);
-console.log(carros.shift());
-console.log(carros);
-console.log(carros.reverse());
+// console.log(carros.pop());
+// console.log(carros);
+// console.log(carros.shift());
+// console.log(carros);
+// console.log(carros.reverse());
+
+// console.log(carros);
+// console.log(carros.splice(2,0, 'Fusca'));
+// console.log(carros);
+
+const transporte1 = ['Barco', 'Aviao'];
+const transporte2 = ['Carro', 'Moto'];
+
+
+const linguagens = ['html', 'css', 'js', 'php', 'python'];
+
+// linguagens.includes('css'); // true
+// linguagens.includes('ruby'); // false
+// linguagens.indexOf('js'); // 2
+
+
+// let htmlString = '<h2>Título Principal</h2>';
+// htmlString = htmlString.split('h2');
+// htmlString = htmlString.join('h3');
+
+// console.log(htmlString);
+
+console.log(linguagens.slice(2));
+
+cloneLinguagens = linguagens.slice();
+console.log(cloneLinguagens); // Tecnica comumente utilizada para clonar arrays
