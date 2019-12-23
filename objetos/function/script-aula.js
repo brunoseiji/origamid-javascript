6a91a5e6036fd3cb77838eea38e4ded43feffaa9
Bruno Seiji <bruno_seiji_1995@hotmail.com>
2019-12-23 13:09:54 -0300

    Functions 2

---
+++ objetos/function/script-aula.js
@@ -0,0 +1,35 @@
+// Retorne a soma total de caracteres dos parágrafos acima utilizando reduce
+
+const paragrafos = document.querySelectorAll('p');
+
+const totalCaracteres = Array.prototype.reduce.call(paragrafos, (acumulador, item) => {
+
+    return acumulador +item.innerText.length;
+}, 0)
+
+console.log(totalCaracteres); // 656
+
+
+// Crie uma função que retorne novos elementos html, com os seguintes parâmetros
+// tag, classe e conteúdo.
+
+function criarElemento(tag, classe, conteudo) {
+    const elemento = document.createElement(tag);
+    classe ? elemento.classList.add(classe) : null;
+    conteudo ? elemento.innerHTML = conteudo : null;
+    return elemento;
+}
+
+console.log(criarElemento('li', 'azul', 'Esse é o conteúdo'));
+
+
+// Crie uma nova função utilizando a anterior como base
+// essa nova função deverá sempre criar h1 com a classe título.
+// Porém o parâmetro conteúdo continuará dinâmico
+
+const h1Titulo = criarElemento.bind(null, 'h1', 'titulo');
+
+const cursosJS = h1Titulo('Cursos de Javascript');
+const cursosHTML = h1Titulo('Cursos de HTML');
+
+console.log(cursosJS, cursosHTML);
